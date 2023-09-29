b89daa5c67c2cafdbec6ad315d7127c50f9a32d5
Atap23 <antonioalvareztorres.trabajo@gmail.com>
2023-09-29 23:46:58 +0200

    [Api Table Utilities] Version 0.0.1
    · Set up project

---
+++ projects/api-table-utilities/src/public-api.ts
@@ -0,0 +1,8 @@
+/*
+ * Public API Surface of api-table-utilities
+ */
+
+export * from './lib/collection-sort';
+export * from './lib/collection-filter';
+export * from './lib/collection-pagination';
+export * from './lib/collection-manager';
